d234ed10b2b9774701e715374e55922a1018781d
Junyan <93970227+junyan21@users.noreply.github.com>
2021-12-08 01:54:32 +0900

    tsでのシェーダーファイルglslの読み込みのためにraw-loaderを追加
    - see: https://qiita.com/yukiTTT/items/0827e39bcdb8ced681aa

--- webpack.config.js
+++ webpack.config.js
@@ -21,6 +21,13 @@ module.exports = {
         test: /\.ts$/,
         // TypeScript をコンパイルする
         use: "ts-loader"
+      },
+      {
+        test: /\.(glsl|vs|fs|vert|frag)$/,
+        exclude: /node_modules/,
+        use: [
+          'raw-loader',
+        ]
       }
     ]
   },
